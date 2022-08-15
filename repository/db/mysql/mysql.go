fa9216b8007798b91d8f13751001384d005589f9
h4ckit <dammysteve10@gmail.com>
2022-08-15 14:46:21 +0100

    [add] Implemented MySQL Functions

--- repository/db/mysql/mysql.go
+++ repository/db/mysql/mysql.go
@@ -16,24 +16,78 @@ func NewMySQLHandler() (*MySqlRepo, error) {
 	conf := config.GetConfig().DB
 	dbRepo := new(MySqlRepo)
 	var err error
-	dsn := fmt.Sprintf("%s:%s@%s(%s;%s)/%s", conf.UserName, conf.Password, "tcp", conf.IP, conf.Port, conf.DBName)
+	dsn := fmt.Sprintf("%s:%s@%s(%s:%s)/%s?parseTime=true", conf.UserName, conf.Password, "tcp", conf.IP, conf.Port, conf.DBName)
 	dbRepo.conn, err = sql.Open("mysql", dsn)
 
 	if err != nil {
 		return nil, err
 	}
 
-	return dbRepo, nil
+	return dbRepo, dbRepo.conn.Ping()
 }
 
-func (m *MySqlRepo) SaveEntry(userId int, entry *model.Spending) error {
-	return nil
+func (m *MySqlRepo) SaveEntry(userId int, entry model.Spending) error {
+	statement := `INSERT INTO spending (messageID, userID, amount, category, createdAt, note) VALUES (?, ?, ?, ?, ?, ?)`
+	_, err := m.conn.Exec(statement, entry.MessageID, userId, entry.Amount, entry.Category, entry.CreatedAt, entry.Note)
+	return err
 }
 
 func (m *MySqlRepo) RetrieveSpending(userId int, start, end string) ([]model.Spending, error) {
-	return []model.Spending{}, nil
+	var result []model.Spending
+
+	var (
+		statement string
+		args      []any
+	)
+
+	args = append(args, userId)
+
+	if start == end {
+		statement = `SELECT amount, category, createdAt, note FROM spending WHERE userID = ? AND DATE(createdAt) = ?`
+		args = append(args, start)
+	} else {
+		statement = `SELECT amount, category, createdAt, note FROM spending WHERE userID = ? AND createdAt >= ? AND createdAt <= ?`
+		args = append(args, start, end)
+	}
+
+	rows, err := m.conn.Query(statement, args...)
+
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		var res model.Spending
+
+		if err := rows.Scan(&res.Amount, &res.Category, &res.CreatedAt, &res.Note); err != nil {
+			return nil, err
+		}
+
+		result = append(result, res)
+	}
+
+	return result, nil
 }
 
 func (m *MySqlRepo) RetrieveSpendingByCategory(userId int, category string) ([]model.CategorySpending, error) {
-	return []model.CategorySpending{}, nil
+	var result []model.CategorySpending
+
+	statement := `SELECT SUM(amount), category FROM spending WHERE userID = ? GROUP BY category`
+
+	rows, err := m.conn.Query(statement, userId)
+
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		var res model.CategorySpending
+
+		if err := rows.Scan(&res.TotalSpending, &res.Category); err != nil {
+			return nil, err
+		}
+
+		result = append(result, res)
+	}
+	return result, nil
 }
