bcf7656a0fc88ab4773628b4cc766d9f8a7e54e3
h4ckit <dammysteve10@gmail.com>
2022-08-15 14:50:08 +0100

    [add] Changed `SaveEntry` Method Signature

--- repository/repo_interface.go
+++ repository/repo_interface.go
@@ -5,7 +5,7 @@ import (
 )
 
 type Repo interface {
-	SaveEntry(userId int, entry *model.Spending) error
+	SaveEntry(userId int, entry model.Spending) error
 	RetrieveSpending(userId int, start, end string) ([]model.Spending, error)
 	RetrieveSpendingByCategory(userId int, category string) ([]model.CategorySpending, error)
 }
