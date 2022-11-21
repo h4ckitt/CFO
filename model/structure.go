c3a027f9d736ab21db5bc73b6d68fd09c471db85
h4ckit <dammysteve10@gmail.com>
2022-11-21 15:14:08 +0100

    [add] Implemented Pending Reply Cache

--- model/structure.go
+++ model/structure.go
@@ -14,3 +14,27 @@ type CategorySpending struct {
 	Category      string
 	TotalSpending float64
 }
+
+type PendingReplyCache map[int]*Spending
+
+func NewPendingReplyCache() PendingReplyCache {
+	return make(map[int]*Spending)
+}
+
+func (p PendingReplyCache) CacheSpending(userID int, spend *Spending) {
+	p[userID] = spend
+}
+
+func (p PendingReplyCache) RetrieveSpending(userID int) *Spending {
+	return p[userID]
+}
+
+func (p PendingReplyCache) HasCachedSpending(userID int) bool {
+	_, exists := p[userID]
+
+	return exists
+}
+
+func (p PendingReplyCache) DeleteCachedSpending(userID int) {
+	delete(p, userID)
+}
