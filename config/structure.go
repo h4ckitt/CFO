3fe6c73cb04db1fde340970a52fc74cca7787b0f
h4ckit <dammysteve10@gmail.com>
2022-11-22 10:21:21 +0100

    [update] Switched To Conform To New Postgresql Repo

--- config/structure.go
+++ config/structure.go
@@ -1,11 +1,20 @@
 package config
 
 type config struct {
-	DB         MySQLDatabase
+	DB         PostgresDatabase
 	TBotAPIKey string
 	PORT       string
 }
 type MySQLDatabase struct {
+	DBName   string
+	Port     string
+	Password string
+	IP       string
+	UserName string
+	Wait     bool
+}
+
+type PostgresDatabase struct {
 	DBName   string
 	Port     string
 	Password string
